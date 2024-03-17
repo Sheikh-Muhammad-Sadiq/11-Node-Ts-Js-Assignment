1355ee983e442cd833e9f65d36e9c30198063167
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:46:05 -0700

    Add files via upload
    Q 11 Node.js of 45 Assignments

---
+++ Q11.ts
@@ -0,0 +1,10 @@
+// 11. Name: Store the names of a few of your freinds in a array called names. Print each person's name 
+// by accessing each element in the list, one at a time.
+
+
+let names: string[] = ["Sadiq", "Salman", "Hamza", "Rehman"]
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
